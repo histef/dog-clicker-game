aa32cfbad6a1f42291ac32aa2a56d0569a5f2dde
Stefanie Chin <hi.stef.c@gmail.com>
2018-06-25 09:47:28 -0700

    add timer that moves dog's location

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const dog = document.querySelector('.dog');
 let count = 0;
 
+//randomly choose dog location
 function moveDog(){
 	dog.style.bottom = Math.floor(Math.random()*(350-35)+35) + 'px';
-	dog.style.left = Math.floor(Math.random()*(900-200)+200) + 'px';
-	count++;
+	dog.style.left = Math.floor(Math.random()*(850-200)+200) + 'px';
 }
 
+//Count tracker
 function counter(){
 	document.querySelector('.count').textContent = count;
 }
 
+//timer that changes dogs location
+function changeLocation(){
+	setInterval(function(){
+		moveDog();
+		changeSize();
+	}, 4000);
+}
+
+//Change size of dog depending on location
 function changeSize(){
 	if (dog.style.bottom <= '350px' && dog.style.bottom > '275px'){
 		return dog.style.width = '50px';
@@ -23,9 +33,12 @@ function changeSize(){
 	}
 }
 
+//Running the game
 dog.addEventListener('click', function(){
 	moveDog();
 	counter();
 	changeSize();
-	console.log(dog.style.bottom);
+	count++;
 });
+
+changeLocation();

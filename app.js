a902b76200689e6a501fc7d1ba75a359dc7eefeb
Stefanie Chin <hi.stef.c@gmail.com>
2018-06-25 16:15:52 -0700

    add dog message function

--- app.js
+++ app.js
@@ -33,11 +33,34 @@ function changeSize(){
 	}
 }
 
+const dogMessage = ['hiya', 'yeah!', 'you rule!', 'got me', 'bark'];
+
+function dogTalk(){
+	const talk= document.querySelector('.talk');
+	//get dogMessage
+	talk.textContent = dogMessage[Math.floor(Math.random()*dogMessage.length)];
+	//toggle css class
+	talk.style.visibility = 'visible';
+    setTimeout(function() {
+        talk.style.visibility = 'hidden';
+    }, 750);
+    //set location
+    let dogY = dog.style.bottom;
+    let getYNumber = (dogY.match(/\d/g)).join('');
+    getYNumber = Number(getYNumber) + 75;
+    talk.style.bottom = (getYNumber + 'px');
+    let dogX = dog.style.left;
+    let getXNumber = (dogX.match(/\d/g)).join('');
+    getXNumber = Number(getXNumber) - 75;
+    talk.style.left = (getXNumber + 'px');
+}
+
 //Running the game
 dog.addEventListener('click', function(){
 	moveDog();
 	counter();
 	changeSize();
+	dogTalk();
 	count++;
 });
 

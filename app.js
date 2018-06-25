4e533b8b947f641f3b39642f75eff2b7f6474e45
Stefanie Chin <hi.stef.c@gmail.com>
2018-06-24 21:20:01 -0700

    add change dog size function

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const dog = document.querySelector('.dog');
 let count = 0;
-//TODO: resize dog depending on what ylevel he is on.
 
-//TODO: when dog is clicked, count clicks & move dog.
 function moveDog(){
-	dog.style.bottom = Math.floor(Math.random()*(350-100)+100) + 'px';
-	dog.style.left = Math.floor(Math.random()*(950-200)+200) + 'px';
+	dog.style.bottom = Math.floor(Math.random()*(350-35)+35) + 'px';
+	dog.style.left = Math.floor(Math.random()*(900-200)+200) + 'px';
 	count++;
 }
 
@@ -13,7 +11,21 @@ function counter(){
 	document.querySelector('.count').textContent = count;
 }
 
+function changeSize(){
+	if (dog.style.bottom <= '350px' && dog.style.bottom > '275px'){
+		return dog.style.width = '50px';
+	} else if (dog.style.bottom <= '275px' && dog.style.bottom > '150px'){
+		return dog.style.width = '100px';
+	} else if (dog.style.bottom <= '150px' && dog.style.bottom > '120px'){
+		return dog.style.width = '150px';
+	} else {
+		return dog.style.width = '200px';
+	}
+}
+
 dog.addEventListener('click', function(){
 	moveDog();
 	counter();
+	changeSize();
+	console.log(dog.style.bottom);
 });
